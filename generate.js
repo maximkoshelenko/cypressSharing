71a5d2268fb4e433125d90faaa931dc2013d42d3
Maksim Koshelenko <maxim.koshelenko@gmail.com>
2019-08-03 22:24:01 +0300

    FIX multiple JSON bug

--- generate.js
+++ generate.js
@@ -1,20 +1,19 @@
 
 var reporter = require('cucumber-html-reporter');
 
-
 var options = {
     theme: 'bootstrap',
-    jsonFile: './cypress/reports/cucumber/combined.json',
+    jsonDir: './cypress/reports/cucumber/',
     output: './cypress/reports/cucumber/report.html',
     reportSuiteAsScenarios: true,
     launchReport: true,
     metadata: {
-        "App Version": "0.3.2",
-        "Test Environment": "STAGING",
-        "Browser": "Chrome  54.0.2840.98",
+        "App Version": "1.0.0",
+        "Test Environment": "PROD",
+        "Browser": "Chrome 75.0.3770.142",
         "Platform": "Windows 10",
         "Parallel": "Scenarios",
-        "Executed": "Remote"
+        "Executed": "LOCAL"
     }
 };
 
